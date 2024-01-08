f9fea121952f2b267954b046538946ed79d94c2d
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-01-09 00:01:19 +0100

    add all link and script nedded

--- views/_layout.php
+++ views/_layout.php
@@ -4,18 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.x/dist/tailwind.min.css" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= ucfirst($page) ?></title>
-    <link rel="stylesheet" href="<?= PATH ?>assets/css/style.css">
 </head>
 <body>
-    <h1><?= ucfirst($page) ?> View</h1>
 
-    <main>
-        <?php include_once 'views/' . $page . '_view.php'; ?>
-    </main>
+<main>
+    <?php include_once 'views/' . $page . '_view.php'; ?>
+</main>
 
-    <footer></footer>
-    <script src="<?= PATH ?>assets/js/main.js"></script>
+<footer></footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="<?= PATH ?>assets/js/main.js"></script>
 </body>
 </html>
