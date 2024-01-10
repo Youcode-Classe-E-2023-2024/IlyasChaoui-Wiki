639822c5a97d971055aa91590c21527895f53f5b
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-01-11 00:12:26 +0100

    Update home_controller.php

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -1,3 +1,14 @@
 <?php
 
+if (isset($_SESSION["login"])) {
+    $userData = User::getUser($_SESSION["user_id"]);
+//    dd($userData);
+}
+
+
+if (isset($_POST['logout'])) {
+    $logout = new User($_SESSION["user_id"]);
+    $logout->logout();
+}
+
 
