c2a31a1ce03429903b99aa57d7004dc213bad7d8
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-01-12 02:29:50 +0100

    add traitment for get categories tags and wikis and also for creating of wikis

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -5,10 +5,23 @@ if (isset($_SESSION["login"])) {
 //    dd($userData);
 }
 
-
 if (isset($_POST['logout'])) {
     $logout = new User($_SESSION["user_id"]);
     $logout->logout();
 }
+
+$categories = category::getCategories();
+
+$tags = Tag::getTags();
+
+if (isset($_POST["create_wiki"])) {
+    extract($_POST);
+    $date = date("U");
+    Wiki::addWiki($title, $description, $tag, $category, $_SESSION["user_id"], $date);
+    unset($_POST);
+    header("location: index.php?page=home");
+}
+
+$wikis = Wiki::getWikis();
 
 
