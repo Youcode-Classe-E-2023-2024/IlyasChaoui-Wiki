4fc2f4aa5d04b20e652f660a9415e0bd9f65aca7
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-01-12 22:23:39 +0100

    add validation form for add tag and category

--- controllers/dashboard_controller.php
+++ controllers/dashboard_controller.php
@@ -16,42 +16,61 @@ if (isset($_POST['logout'])) {
 
 $tags = Tag::getTags();
 
-if(isset($_GET["tag_id"])) {
+if (isset($_GET["tag_id"])) {
     $tagId = filter_input(INPUT_GET, "tag_id", FILTER_SANITIZE_SPECIAL_CHARS);
     Tag::deleteTag($tagId);
     header("location: index.php?page=dashboard&add_tag=true");
 }
 
-if(isset($_POST["editTag"])) {
+if (isset($_POST["editTag"])) {
     $tag = filter_input(INPUT_POST, "tag", FILTER_SANITIZE_SPECIAL_CHARS);
     $tagId = filter_input(INPUT_POST, "tag_id", FILTER_SANITIZE_SPECIAL_CHARS);
+//    dd($tagId);
     Tag::updateTag($tag, $tagId);
     header("location: index.php?page=dashboard&add_tag=true");
 }
 
-if(isset($_POST["add_tag"])) {
+if (isset($_POST["add_tag"])) {
     $tag = filter_input(INPUT_POST, "tag", FILTER_SANITIZE_SPECIAL_CHARS);
-    Tag::addTag($tag);
+
+    if (Validation::validateTag(strtolower($tag))) {
+        $msg = Validation::validateTag($tag);
+        http_response_code(400);
+        echo json_encode(["error" => $msg]);
+    } else {
+        Tag::addTag($tag);
+        http_response_code(200);
+        echo json_encode(["success" => "Tag added successfully"]);
+    }
+
     exit;
 }
 
 $categories = category::getCategories();
 
-if(isset($_POST["add_category"])) {
+if (isset($_POST["add_category"])) {
     $category = filter_input(INPUT_POST, "category", FILTER_SANITIZE_SPECIAL_CHARS);
-    Category::addCategory($category);
-    echo "category added successfully";
+    $category = strtolower($category);
+    if (Validation::validateCategory($category)) {
+        $msg = Validation::validateCategory($category);
+        http_response_code(400);
+        echo json_encode(["error" => $msg]);
+    } else {
+        Category::addCategory($category);
+        http_response_code(200);
+        echo json_encode(["success" => "Category added successfully"]);
+    }
     exit;
 }
 
-if(isset($_POST["editCategory"])) {
+if (isset($_POST["editCategory"])) {
     $category = filter_input(INPUT_POST, "category", FILTER_SANITIZE_SPECIAL_CHARS);
     $categoryId = filter_input(INPUT_POST, "category_id", FILTER_SANITIZE_SPECIAL_CHARS);
     Category::updateCategory($category, $categoryId);
     header("location: index.php?page=dashboard&add_category=true");
 }
 
-if(isset($_GET["category_id"])) {
+if (isset($_GET["category_id"])) {
     $categoryId = filter_input(INPUT_GET, "category_id", FILTER_SANITIZE_SPECIAL_CHARS);
     Category::deleteCategory($categoryId);
     header("location: index.php?page=dashboard&add_category=true");
