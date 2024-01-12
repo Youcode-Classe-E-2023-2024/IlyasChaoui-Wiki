36e39d32defd8292985da50a9b83630cd89db91e
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-01-12 22:12:20 +0100

    fix and add rejex for forms

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -39,169 +39,111 @@ product.addEventListener("click", () => {
 //
 const addTagBtn = document.getElementById("addTagBtn");
 const addTagInput = document.getElementById("tag-input");
-const editTagInput = document.getElementById("edit-tag-input");
-const tagSection = document.getElementById("tag-section");
-let currentTagId;
-let currentTag;
 
 addTagBtn.addEventListener("click", (event) => {
     console.log("test");
     event.preventDefault();
     if (addTagInput.value.length !== 0) {
-        $.post(
-            "index.php?page=dashboard",
-            {
+        $.ajax({
+            type: "post",
+            url: "index.php?page=dashboard",
+            data: {
                 add_tag: true,
                 tag: addTagInput.value
             },
-            (data) => {
+            success: (data) => {
+                const response = JSON.parse(data);
+                if (response.success) {
+                    console.log(response.success);
+                    document.querySelector("#modal_body").classList.add("hidden");
+                    addTagInput.style.border = "";
+                    addTagInput.value = "";
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: "top",
+                        showConfirmButton: false,
+                        timer: 3000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                            toast.onmouseenter = Swal.stopTimer;
+                            toast.onmouseleave = Swal.resumeTimer;
+                        }
+                    });
+                    Toast.fire({
+                        icon: "success",
+                        title: response.success
+                    });
+                    setTimeout(() => {
+                        window.location.href = "index.php?page=dashboard&add_tag=true";
+                    }, 3000);
+
+                }
+            },
+            error: (error) => {
+                var data = JSON.parse(error.responseText);
                 console.log(data);
-                document.querySelector("#modal_body").classList.add("hidden");
-                addTagInput.style.border = ""
-                addTagInput.value = "";
-                window.location.href = "index.php?page=dashboard&add_tag=true";
+                Swal.fire({icon: "error", title: "Error", text: data.error});
             }
-        )
+        })
     } else
-        addTagInput.style.border = "1px solid red";
+        addTagInput.style.border = "2px solid red";
 })
 
 const addCategoryBtn = document.getElementById("addCategoryBtn");
 const addCategoryInput = document.getElementById("category-input");
-const editCategoryInput = document.getElementById("edit-category-input");
-const categorySection = document.getElementById("category-section");
-let currentCategoryId;
-let currentCategory;
 
 addCategoryBtn.addEventListener("click", (event) => {
     event.preventDefault();
     console.log("test");
     if (addCategoryInput.value.length !== 0) {
-        $.post(
-            "index.php?page=dashboard",
-            {
+        $.ajax({
+            type: "post",
+            url: "index.php?page=dashboard",
+            data: {
                 add_category: true,
                 category: addCategoryInput.value
             },
-            (data) => {
-                console.log(data);
+            success: (data) => {
+                const response = JSON.parse(data);
+                console.log(response);
                 addCategoryInput.style.border = ""
                 addCategoryInput.value = "";
-                window.location.href = "index.php?page=dashboard&add_category=true";
-
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.onmouseenter = Swal.stopTimer;
+                        toast.onmouseleave = Swal.resumeTimer;
+                    }
+                });
+                Toast.fire({
+                    icon: "success",
+                    title: response.success
+                });
+                setTimeout(() => {
+                    window.location.href = "index.php?page=dashboard&add_category=true";
+                }, 3000);
+            },
+            error: (error) => {
+                var data = JSON.parse(error.responseText);
+                console.log(data);
+                Swal.fire({icon: "error", title: "Error", text: data.error});
             }
-        )
+        })
     } else
-        addCategoryInput.style.border = "1px solid red";
+        addCategoryInput.style.border = "3px solid red";
 })
-//
-// // function getCategories() {
-// //     categorySection.innerHTML = "";
-// //     $.get(
-// //         "index.php?page=dashboard&get_categories=true",
-// //         (data) => {
-// //             console.log(data);
-// //             let categories = JSON.parse(data);
-// //
-// //             categories.forEach((category) => {
-// //                 categorySection.innerHTML += `<tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
-// //                                             <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">${category.category_id}</td>
-// //                                             <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">${category.category}</td>
-// //                                             <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-// //                                                 <a href="#category-form" class="edit-category text-blue-600 dark:text-blue-500 hover:underline"
-// //                                                 data-category-id="${category.category_id}" data-category-name="${category.category}">Edit</a>
-// //                                             </td>
-// //                                             <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
-// //                                                 <a data-category-id="${category.category_id}" class="delete-category cursor-pointer text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
-// //                                             </td>
-// //                                         </tr>`;
-// //             })
-// //         }
-// //     )
-// //
-// //     $(categorySection).ready(() => {
-// //         $(categorySection).on("click", ".edit-category", function () {
-// //             currentCategoryId = $(this).data("category-id");
-// //             currentCategory = $(this).data("category-name");
-// //             console.log(document.querySelector("#edit-category-form").classList);
-// //             document.querySelector("#edit-category-form").classList.remove("hidden");
-// //             document.querySelector("#category-form").classList.add("hidden");
-// //             document.querySelector("#edit-category-input").value = currentCategory;
-// //         })
-// //
-// //         $(categorySection).on("click", ".delete-category", function () {
-// //             currentCategoryId = $(this).data("category-id");
-// //             deleteCategory(currentCategoryId);
-// //         });
-// //     })
-// //
-// //
-// // }
-//
-// function updateCategory(categoryId) {
-//     event.preventDefault();
-//     if (editCategoryInput.value.length !== 0) {
-//         $.post(
-//             "index.php?page=dashboard",
-//             {
-//                 edit_category: true,
-//                 category: editCategoryInput.value,
-//                 category_id: categoryId
-//             },
-//             (data) => {
-//                 console.log(data);
-//                 editCategoryInput.style.border = ""
-//                 editCategoryInput.value = "";
-//             }
-//         )
-//     } else
-//         editCategoryInput.style.border = "1px solid red";
-// }
-//
-// function deleteCategory(categoryId) {
-//     $.post(
-//         "index.php?page=dashboard",
-//         {
-//             category_id: categoryId,
-//             delete_category: true
-//         },
-//         (data) => {
-//             console.log(data);
-//             getCategories();
-//         }
-//     )
-// }
-//
-// // getCategories();
-//
-// function showForm() {
-//     document.querySelector("#edit-category-form").classList.add("hidden");
-//     document.querySelector("#category-form").classList.remove("hidden");
-// }
-//
-//
-// function runWhenJQueryLoaded() {
-//     if (window.jQuery) {
-//         $("#editCategoryBtn").on("click", () => {
-//             updateCategory(currentCategoryId);
-//             // getCategories();
-//             getTags();
-//         })
-//     } else {
-//         // Check again after a short delay
-//         setTimeout(runWhenJQueryLoaded, 50);
-//     }
-// }
-//
-// runWhenJQueryLoaded();
-//
+
 
 const editBtn = document.querySelectorAll(".editBtn");
 const inputTag = document.getElementById("editInput");
 const inputTagId = document.getElementById("editTagId");
 
-editBtn.forEach(function(button) {
-    button.addEventListener("click", function() {
+editBtn.forEach(function (button) {
+    button.addEventListener("click", function () {
         inputTag.value = this.getAttribute("data-tag-name");
         inputTagId.value = this.getAttribute("data-tag-id");
     });
@@ -213,8 +155,8 @@ const inputCategory = document.getElementById("categoryEditInput");
 const inputCategoryId = document.getElementById("editCategoryId");
 
 
-categoryEditBtn.forEach(function(button) {
-    button.addEventListener("click", function() {
+categoryEditBtn.forEach(function (button) {
+    button.addEventListener("click", function () {
         console.log(this.getAttribute("data-category-name"));
         inputCategory.value = this.getAttribute("data-category-name");
         inputCategoryId.value = this.getAttribute("data-category-id");
